a8c5d1adc9a62771a61c4bb3103939876496f1a8
gbarogo <36666067+gbarogo@users.noreply.github.com>
2020-01-22 09:19:51 +0100

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -6,9 +6,10 @@ import scipy.stats as st
 
 #GMM
 class GMM:
-    #Estado inicial
-    def __init__(self,data, X,classes,iterations):
+    #Initial state
+    def __init__(self,data,target, X,classes,iterations):
         self.data =data
+        self.target=target
         self.iterations = iterations
         self.classes = classes
         self.X = X
@@ -20,7 +21,7 @@ class GMM:
         indexes = np.random.randint(len(self.data), size=int(len(self.data)*0.1))
         temp = dict()
         for i in indexes:
-            k = digits_target[i]
+            k = self.target[i]
             pixels = self.data[i]
             if k not in temp:
                 temp[k] = list()
@@ -38,12 +39,12 @@ class GMM:
             self.theta[k] = np.array([pi, mu, cov])
                
         r = self.E_step()
-        self.theta = self.M_step(r)
+        self.M_step(r)
         for i in range(self.iterations):               
             """E Step"""
             r = self.E_step()
             """M Step"""
-            self.theta = self.M_step(r)
+            self.M_step(r)
     
     def E_step(self):
         r = np.zeros((self.X.shape[0],self.classes)) 
@@ -55,15 +56,14 @@ class GMM:
         return r
 
     def M_step(self, r):
-        r_k = {k:np.sum(r[:,k]) for k in range(10)}
-        self.theta = dict()
+        r_k = {k:np.sum(r[:,k]) for k in range(self.classes)}
+        #self.theta = dict()
         epsilon = 0.01
         for k in r_k:
             pi = r_k[k]/len(self.X)
             mu = np.sum([r[i][k]*self.X[i] for i in range(len(self.X))], axis=0)/r_k[k]
             cov = np.sum([r[i][k]*(self.X[i]**2) for i in range(len(self.X))], axis=0)/r_k[k] - mu**2 + epsilon
             self.theta[k] = np.array([pi, mu, cov])
-        return self.theta 
 
     def predict(self, X):
         probs = np.zeros((len(X), self.classes))
@@ -105,20 +105,23 @@ print(y_train.shape)
 x_train /= 16
 x_test /= 16
 
-GMM = GMM(data,x_train,10,50)     
+#Initialize the class
+GMM = GMM(data,digits_target,x_train,10,50)     
 GMM.fit()
 
+#Prediction Over Train Data
 y_pred_train = GMM.predict(x_train)
 
-print("Classification report EM:\n%s\n" % 
+print("Classification report EM (y_train, y_pred_train):\n%s\n" % 
       (metrics.classification_report(y_train, y_pred_train)))
-print("Confusion matrix EM:\n%s" % metrics.confusion_matrix(y_train, y_pred_train))
+print("Confusion matrix EM (y_train, y_pred_train):\n%s" % metrics.confusion_matrix(y_train, y_pred_train))
 print()
 h_c_v = metrics.homogeneity_completeness_v_measure(y_train, y_pred_train)
 print('Homogenity:',h_c_v[0])
 print('Completeness:',h_c_v[1])
 print('V-measure:',h_c_v[2])
 
+#Prediction Over Test Data
 y_pred_test = GMM.predict(x_test)
 
 print("Classification report EM:\n%s\n" % 
@@ -130,7 +133,7 @@ print('Homogenity:',h_c_v[0])
 print('Completeness:',h_c_v[1])
 print('V-measure:',h_c_v[2])
 
-
+#Repair Over Train Data
 y_pred_repair = GMM.repair(y_train, y_pred_train)
 print("Classification report SKLearn K-Means:\n%s\n" % 
       (metrics.classification_report(y_train, y_pred_repair)))
@@ -142,6 +145,7 @@ print('Completeness:',h_c_v[1])
 print('V-measure:',h_c_v[2])
 print()
 
+#Repair Over Test Data
 y_pred_test_repair = GMM.repair(y_test, y_pred_test)
 print("Classification report SKLearn k-Means:\n%s\n" % 
       (metrics.classification_report(y_test, y_pred_test_repair)))
@@ -152,10 +156,11 @@ print('Homogenity:',h_c_v[0])
 print('Completeness:',h_c_v[1])
 print('V-measure:',h_c_v[2])
 
-
+#k-Means
 k_means = cluster.KMeans(n_clusters=10).fit(x_train)
 y_pred_kmeans = k_means.predict(x_train)
 y_pred_kmeans_real = GMM.repair(y_train, y_pred_kmeans)
+
 print("Classification report SKLearn K-Means:\n%s\n" % 
       (metrics.classification_report(y_train, y_pred_kmeans_real)))
 print("Confusion matrix SKLearn EM:\n%s" % metrics.confusion_matrix(y_train, y_pred_kmeans_real))
